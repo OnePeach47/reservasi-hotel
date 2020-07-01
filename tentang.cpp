40f9d6d27d641d3185c1df7c53faa9a8577d1a03
sanmade <67454815+sanmade@users.noreply.github.com>
2020-07-01 12:24:10 +0700

    Update tentang.cpp

--- tentang.cpp
+++ tentang.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 #include "header.h"
+#include <windows.h>
 
 using namespace std;
 
 void tentang(){
-    cout << endl;
+    cout << "Program ini dikembangkan oleh :";
+    cout<<endl;
+    cout << "Afifah Nuraeni"<<endl;
+    cout << "Jihad Shindu Ghossa"<<endl;
+    cout << "Rozan Fajri Irfani"<<endl;
+    cout << "Santho Madear Purba"<<endl;
+ system("pause");
+ main();
 }
 
 void keluar(){
-    freeMemory();
+    cout << "Terima Kasih Sudah Menggunakan Layanan Kami"<<endl;
+    cout << "Selamat Menikmati,Semoga Hari Anda Menyenangkan :)"<<endl;
+    cout << "Sampai Jumpa :)"<<endl;
+    cout << endl;
+
+    char pil;
+    cout<< "Yakin ingin keluar(Y/T)? ";
+    cin >> pil;
+    if (pil == 'Y' || 'y'){
+        cout << "Terima Kasih Sudah Menggunakan Layanan Kami"<<endl;
+        exit(1);
+    }else if (pil == 'T'|| 't'){
+    main();
+    }
 }
+
+
